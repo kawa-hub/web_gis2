c19318fe27fe20b6c0df3e63d09c854b583c912a
kawa-hub <harukawa1216@gmail.com>
2026-03-14 15:14:23 +0900

    Delete save.php

--- save.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = file_get_contents("php://input");
-$js = "var markerData = " . json_encode(json_decode($data), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . ";";
-
-file_put_contents("data.js", $js);
-
-echo "保存しました";
-
