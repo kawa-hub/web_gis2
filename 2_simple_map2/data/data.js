024dd24ba0b7960c4e8f826f7ec4b469797d376d
kawa-hub <harukawa1216@gmail.com>
2026-03-14 15:15:41 +0900

    Add files via upload

---
+++ 2_simple_map2/data/data.js
@@ -0,0 +1,18 @@
+var locations = {
+"type": "FeatureCollection",
+"name": "data",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "lon": 144.2688554, "lat": 43.59032139, "id": 1, "地点名": "屈斜路湖" }, "geometry": { "type": "Point", "coordinates": [ 144.2688554, 43.59032139 ] } },
+{ "type": "Feature", "properties": { "lon": 144.5038259, "lat": 43.56181135, "id": 2, "地点名": "摩周湖" }, "geometry": { "type": "Point", "coordinates": [ 144.50382590000001, 43.56181135 ] } },
+{ "type": "Feature", "properties": { "lon": 144.1196113, "lat": 43.43242995, "id": 3, "地点名": "阿寒湖" }, "geometry": { "type": "Point", "coordinates": [ 144.1196113, 43.43242995 ] } },
+{ "type": "Feature", "properties": { "lon": 142.2367409, "lat": 44.30444861, "id": 4, "地点名": "朱鞠内湖" }, "geometry": { "type": "Point", "coordinates": [ 142.2367409, 44.30444861 ] } },
+{ "type": "Feature", "properties": { "lon": 143.1990035, "lat": 43.38099703, "id": 5, "地点名": "糠平湖" }, "geometry": { "type": "Point", "coordinates": [ 143.1990035, 43.38099703 ] } },
+{ "type": "Feature", "properties": { "lon": 141.3545374, "lat": 42.73580765, "id": 6, "地点名": "支笏湖" }, "geometry": { "type": "Point", "coordinates": [ 141.3545374, 42.73580765 ] } },
+{ "type": "Feature", "properties": { "lon": 140.7978347, "lat": 42.57823746, "id": 7, "地点名": "洞爺湖" }, "geometry": { "type": "Point", "coordinates": [ 140.79783470000001, 42.57823746 ] } },
+{ "type": "Feature", "properties": { "lon": 140.6897851, "lat": 41.9922203, "id": 8, "地点名": "大沼" }, "geometry": { "type": "Point", "coordinates": [ 140.6897851, 41.9922203 ] } },
+{ "type": "Feature", "properties": { "lon": 144.1767141, "lat": 43.99919933, "id": 9, "地点名": "網走湖" }, "geometry": { "type": "Point", "coordinates": [ 144.1767141, 43.99919933 ] } },
+{ "type": "Feature", "properties": { "lon": 142.0969611, "lat": 43.04707993, "id": 10, "地点名": "シューパロ湖" }, "geometry": { "type": "Point", "coordinates": [ 142.096961099999987, 43.04707993 ] } },
+{ "type": "Feature", "properties": { "lon": 142.6822584, "lat": 42.45388696, "id": 11, "地点名": "高見湖" }, "geometry": { "type": "Point", "coordinates": [ 142.6822584, 42.45388696 ] } }
+]
+}
---
+++ 2_simple_map2/data/image/data.js
@@ -0,0 +1,14 @@
+// data/data.js
+const locations = [
+    {
+        name: "1",
+        lat: 43.1,
+        lon: 142.5
+    },
+    {
+        name: "2",
+        lat: 43.5,
+        lon: 142.9
+    }
+    // 必要に応じて追加
+];
